d59fe986966c0911ad14805dc05830e7d1f32e68
adud <antonin.dudermel@club.fr>
2016-11-12 18:27:09 +0100

    corriger quelques erreurs d'encodage (note : trouver une alternative à MacCaml)

--- speinfo/tp3/automates.ml
+++ speinfo/tp3/automates.ml
@@ -1,8 +1,8 @@
 type automate = {
-  etats : int list ; (* ensemble des Ã©tats *)
-  q0 : int; (* Ã©tat initial *)
-  finaux : int list; (* liste des Ã©tats finaux *)
-  (* la fonction de transition est reprÃ©sentÃ©e par la liste des
+  etats : int list ; (* ensemble des Žtats *)
+  q0 : int; (* Žtat initial *)
+  finaux : int list; (* liste des Žtats finaux *)
+  (* la fonction de transition est reprŽsentŽe par la liste des
   triplets (q, a, q') tels que delta(q, a) = q' : *)
   transitions : (int * char * int) list;
 };;
@@ -10,8 +10,8 @@ exception Blocage;;
 
 
 (*Avant toute chose, je souhaiterais signaler que l'usage d'ensembles
-(set) me semble beaucoup plus approprié que les listes, et que je
-perds une complexité folle à chaque fois qu'il faut éviter des doublons*)
+(set) me semble beaucoup plus appropriŽ que les listes, et que je
+perds une complexitŽ folle ˆ chaque fois qu'il faut Žviter des doublons*)
 
 
 
@@ -28,10 +28,10 @@ let rec assoc3 li q c = match li with
 	    else assoc3 r q c
 ;;  
 (*execute_aux int -> automate -> char list -> int
-Si l'automate aux se trouve dans l'Ã©tat q et qu'on
+Si l'automate aux se trouve dans l'Žtat q et qu'on
 lui fait passer mot,
-execute_aux q aut mot retourne l'Ã©tat de l'automate
-aprÃ¨s que le mot ait Ã©tÃ© passÃ© si l'automate ne
+execute_aux q aut mot retourne l'Žtat de l'automate
+aprÃ¨s que le mot ait ŽtŽ passŽ si l'automate ne
 bloque pas, soulÃ¨ve Blocage sinon *)
   
 let rec execute_aux aut mot q = match mot with
@@ -67,7 +67,7 @@ let test = []
 ;;
 
 (*nbp : int -> char -> bool
-   nbp q c renvoie true si l'automate dans l'Ã©tat q ne
+   nbp q c renvoie true si l'automate dans l'Žtat q ne
    bloque pas Ã  la lecture de la lettre c et false sinon*)
 
 let nbp aut q c =
@@ -77,7 +77,7 @@ let nbp aut q c =
   
 let est_complet alph aut =
   (*ecp : int -> bool
-   ecp q renvoie true si l'automate dans l'Ã©tat q ne bloque jamais
+   ecp q renvoie true si l'automate dans l'Žtat q ne bloque jamais
    Ã  la lecture d'une lettre de alph et false sinon*)
   let ecp q =
     for_all (fun c -> nbp aut q c) alph in
@@ -120,7 +120,7 @@ cartesian
 etendre:('a * 'b * 'c) list -> 'a -> 'b * 'c -> 'a * 'b * 'c
 etendre trans puits (q,c) retourne (q,c,q') si (q,c,q') est dans trans
 ou (q,c,puits) sinon
-i.e. si q.c est défini dans les transitions trans, alors retourne la transition q -(c)-> q' 
+i.e. si q.c est dŽfini dans les transitions trans, alors retourne la transition q -(c)-> q' 
 sinon, retourne la transition q -(c)-> puits*)
 
 let etendre trans puits (q,c) =
@@ -157,7 +157,7 @@ decode p q (code p q (3,0));;
 
 
 (*filter ('a -> bool) -> 'a list -> 'a list
-filter f l retourne la liste des éléments de l vérifiant f*)
+filter f l retourne la liste des ŽlŽments de l vŽrifiant f*)
 let rec filter f l = match l with
 	|[]->[]
 	|r::q -> if f r 
@@ -173,9 +173,9 @@ let produit aut1 aut2 =
 	let initiaux = codage (aut1.q0, aut2.q0) in
 	let F = codlist (cartesian aut1.finaux aut2.finaux) in
 	
-	(*le plus délicat : faire le produit des fonctions de transition
-	ici : faire violament un produit cartésien puis choisir seulement
-	les couples d'états ayant la même lettre*)
+	(*le plus dŽlicat : faire le produit des fonctions de transition
+	ici : faire violemment un produit cartŽsien puis choisir seulement
+	les couples d'Žtats ayant la mme lettre*)
 	let trans1,trans2 = aut1.transitions,aut2.transitions in
 	let fi ((q1,a1,q1'),(q2,a2,q2')) = a1 = a2 in
 	let filtrat = filter fi (cartesian trans1 trans2) in
@@ -191,8 +191,8 @@ let produit aut1 aut2 =
 (*acces_aux : ('a * 'b * 'c) list -> 'c list -> 'c list
 acces_aux [(q1,a,q'1);(q2,b,q'2)...;(qn,k,q'n)] s retourne 
 [q'1;...q'n]@s
-i.e. acces_aux trans s retourne tous les états ciblés par les transitions
-de la liste trans, concaténé à s*)
+i.e. acces_aux trans s retourne tous les Žtats ciblŽs par les transitions
+de la liste trans, concatŽnŽ ˆ s*)
 
 let rec acces_aux trans s = match trans with
 	|[] -> s
@@ -210,8 +210,8 @@ let rec coacces_aux trans s = match trans with
 
 
 (*acces : automate -> int -> int list -> int list
-acces aut l1 retourne tous les états accessibles de aut en une étape à
-partir de l'etat q, le tout concaténé à s*)
+acces aut l1 retourne tous les Žtats accessibles de aut en une Žtape ˆ
+partir de l'etat q, le tout concatŽnŽ ˆ s*)
 
 let acces aut q s =
 	let trans = filter (fun (q1,a,q2) -> q1=q) aut.transitions in
@@ -224,8 +224,8 @@ let coacces aut q s =
 ;;
 
 (*accessibles_aux : automate -> int list -> int list -> int list
-accessibles_aux aut etats s retourne tous les états de aut accessibles en
-une étape à partir de etats, le tout concaténé à s*)
+accessibles_aux aut etats s retourne tous les Žtats de aut accessibles en
+une Žtape ˆ partir de etats, le tout concatŽnŽ ˆ s*)
 
 let rec accessibles_aux aut etats s = match etats with
 	|[] -> s
@@ -241,8 +241,8 @@ let rec coaccessibles_aux aut etats s = match etats with
 
 
 (*accessibles_it : automate -> int list -> int list
-accessibles_it aut etats retourne tous les états accessibles à partir des
-états etats*)
+accessibles_it aut etats retourne tous les Žtats accessibles ˆ partir des
+Žtats etats*)
 
 let rec accessibles_it aut etats =
 	let etats_it = accessibles_aux aut etats etats in
@@ -277,14 +277,14 @@ coaccessibles acc_test
 
 
 (*inter_list 'a list -> 'a list -> 'a list
-inter l1 l2 retourne une liste contenant les éléments présents dans
-l1 et l2 sans garantie sur la multiplicité *)
+inter l1 l2 retourne une liste contenant les ŽlŽments prŽsents dans
+l1 et l2 sans garantie sur la multiplicitŽ *)
 let inter_list l1 l2 =
 	filter (fun x -> mem x l2) l1
 ;;
 
 (*emonde : automate -> automate
-retourne la version émondée d'un automate*)
+retourne la version ŽmondŽe d'un automate*)
 
 let emonde aut = 
 	let Q = inter_list (accessibles aut) (coaccessibles aut) in
@@ -299,14 +299,14 @@ emonde acc_test;;
 	
 
 (*parce que la programmation fonctionnelle ca va bien quelques heures
-mais la plus, une idée fonctionelle est la bienvenue
+mais la plus, une idŽe fonctionelle est la bienvenue
 
 afd_local char list -> char list -> (char*char) list *)
 
 let afd_local p1 s1 f2 =
-	(*crée une table associant à chaque caractère une valeur entière
-	l'état i sera l'état du caractère c tq (c,i) dans table
-	l'automate étant local, pour toute transition (q,c,q'),
+	(*crŽe une table associant ˆ chaque caractère une valeur entière
+	l'Žtat i sera l'Žtat du caractère c tq (c,i) dans table
+	l'automate Žtant local, pour toute transition (q,c,q'),
 	q' = i*)
 	let table = ref [] in
 	let compt = ref 1 in
