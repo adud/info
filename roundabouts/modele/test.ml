8313b6ec3c69c4ad68e2f59d725ff29d69d05ce8
adud <antonin.dudermel@club.fr>
2017-01-07 20:47:48 +0100

    tipe : suppression des anciens jeux de test

--- roundabouts/modele/test.ml
+++ roundabouts/modele/test.ml
@@ -6,73 +6,9 @@ open Affichage
 open Comportements
 ;;
 
-let checkpoint =  creer_inter 1 1 onemany
-;; 
-
-let circuit =  creer_section 166 6
-;;
-
-let rec tour = circuit::tour in
-
-    
-for i=0 to 4 do
-  
-  ajcar circuit (creer_voiture 6 tour) (i*7)
-
-done;
-
-for i=0 to 10 do
-  ajcar circuit (creer_voiture 0 tour) (60 +i)
-done
-;;
-
-  lier checkpoint 0 checkpoint 0 circuit
-;;
-(*test circuit*)
-let () =
-  for i=0 to (-20) do
-    increment circuit;
-    traverser checkpoint i;
-  done;
-  for i=0 to (-20) do
-    print_section circuit;
-    print_newline ();
-    traverser checkpoint i;
-    increment circuit;  
-  done;
-
-  let p = Plateau.construire [circuit] [checkpoint] [] in
-  Plateau.jouer p 0 20
-  ;
-
-(*test one_many*)
-
-  let entr = creer_section 10 3 in
-  let sor1 = creer_section 20 3 in
-  let sor2 = creer_section 20 3 in
-  let a = creer_spawn () in
-
-  let trio = creer_inter 1 2 onemany in
-
-  let itin1 = [sor1;sor1] in
-  let itin2 = [sor2;sor2] in
-  
-
-  lier a 0 trio 0 entr;
-  lier trio 0 (creer_sortie "1") 0 sor1;
-  lier trio 1 (creer_sortie "2") 0 sor2;
-  ajcar entr (creer_voiture 0 itin1) 0;
-  ajcar entr (creer_voiture 0 itin2) 1;
-  
-  let s = [entr;sor1;sor2] in
-  for i = 0 to (-10) do
-    List.iter print_section s;
-    print_newline ();
-    List.iter increment s;
-    traverser trio i;
-  done;
 
 (*test two_one*)
+let () = 
   let ent0 = creer_section 10 3 in
   let ent1 = creer_section 10 3 in
   let sor = creer_section 20 3 in
@@ -91,24 +27,5 @@ let () =
   let sp = Plateau.spawn_car 3 0 0 0 ent0 itin in
   let p = Plateau.construire s [bottle] [sp] in
 
-  (*Plateau.jouer p 0 20;*)
-  
-  for i = 0 to (-20) do
-    List.iter print_section s;
-    if i mod 3 = 0
-    then
-      begin
-	match (observer ent0).(0)
-	with
-	|None -> ajcar ent0 (creer_voiture 0 itin) 0
-	|Some(_) -> ()
-      end
-    else ()
-  ;
-  (*info_fdmal [ent0];*)
-
-  List.iter increment s;
-  traverser bottle i;
-    print_newline ();
-  done;
+  Plateau.jouer p 0 20
 ;;
