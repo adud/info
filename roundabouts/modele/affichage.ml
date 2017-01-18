ef25796f35d9ae96bc5d5cd35d341573b79ed603
adud <antonin.dudermel@club.fr>
2017-01-18 15:24:05 +0100

    tipe : modification diagramme fondamental : tant pis pour la vitesse

--- roundabouts/modele/affichage.ml
+++ roundabouts/modele/affichage.ml
@@ -35,7 +35,7 @@ let info_fdmal lsec =
   let d = densite lsec in
   let v = vitesse_moy lsec in
   List.iter (fun x -> (print_float x;virgule ()))
-            [d;v;d*.v]
+            [d;d*.v]
 ;;
 
 open Graphics
