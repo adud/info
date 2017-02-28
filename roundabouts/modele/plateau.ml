c5f5e09a0b8f31907e99ebaf2ccb30dd095f403e
adud <antonin.dudermel@club.fr>
2017-02-28 20:12:58 +0100

    tipe : modification des fonctions d'information : retournent une liste de flottants

--- roundabouts/modele/plateau.ml
+++ roundabouts/modele/plateau.ml
@@ -13,6 +13,79 @@ type plateau =
   }
 ;;
 
+type rond_point =
+  {ents: section array; sors : section array; rond: section array;
+   diss: distr array}
+
+let rp_ent rp = rp.ents
+;;
+
+let rp_sor rp = rp.sors
+;;
+
+let rp_ron rp = rp.rond
+;;
+
+let rp_dis rp = rp.diss
+;;
+
+    
+let creer_rond_point n l p vmi vme cmp =
+  
+  let sp = creer_spawn () in
+  let dumi = creer_inter 0 passif in
+  let isec = Array.make n dumi in
+  
+  let dums = creer_section 0 0 in
+  let ent = Array.make n dums in
+  let sor = Array.make n dums in
+  let ron = Array.make n dums in
+
+  (*creation des sections et des intersections*)
+  for i=0 to (n-1) do
+    ent.(i) <- creer_section l vme;
+    sor.(i) <- creer_section l vme;
+    ron.(i) <- creer_section p vmi;
+    isec.(i) <- creer_inter 2 cmp;    
+  done;
+
+(*liaisons internes*)
+  for i=0 to n-2 do
+    lier isec.(i) 0 isec.(i+1) 0 ron.(i)
+  done;
+  lier isec.(n-1) 0 isec.(0) 0 ron.(n-1);
+
+(*liaisons externes*)
+  for i=0 to n-1 do
+    lier isec.(i) 0 (creer_sortie "") 1 sor.(i) ;
+    lier sp 1 isec.(i) 1 ent.(i);
+    ajouter_sortie isec.(i) 1 ron.(i)
+  done;
+
+    {ents=ent;
+     sors=sor;
+     rond=ron;
+     diss=isec}
+;;
+
+let faire_itin rp ne ns fin =
+  let inner = rp.rond in
+  let n = Array.length inner in
+  let rec boucle e s li =
+    if e == s
+    then li
+    else if e == n
+    then boucle 0 s li
+    else
+      inner.(e)::boucle (e+1) s li
+  in
+  rp.ents.(ne)::boucle ne ns (rp.sors.(ns)::fin)
+  
+let pl_add_rp pl rp =
+  pl.sects <- List.flatten (List.map Array.to_list [rp.ents;rp.sors;rp.rond]) @ pl.sects;
+  pl.distrs <- Array.to_list rp.diss @ pl.distrs;
+;;
+  
 let construire s d ev = {sects=s;distrs=d;events=ev};;
   
 let iterer p t =
@@ -63,21 +136,22 @@ let faire p i f dbt bent bsor fin =
   Random.self_init ();
   dbt p;
   for t = i to (f-1) do
-    bent p;
+    bent p t;
     iterer p t;
-    bsor p;
+    bsor p t;
   done;
-  fin p;
+  fin p f;
 ;;
 
-let rien (p:plateau) = ();;
+let rien (p:plateau) t = ();;
   
 let silence p i f =
   faire p i f rien rien rien rien
 ;;
 
 let jouer p i f =
-  let aff p =
+  let aff p t =
+    ignore t;
     afficher p;
     print_newline ()
   in
@@ -101,7 +175,8 @@ let patience d =
 ;;
   
 let animer p i f grcr =
-  let bent p =
+  let bent p t =
+    ignore t;
     Graphics.clear_graph ();
     imager p grcr;
     Graphics.synchronize ();
@@ -113,11 +188,17 @@ let animer p i f grcr =
 
 
 let modeliser p i f info =
-  let bent p =
-    info();
+  let bent p t=
+    print_info (info p);
     print_newline ();
   in
   faire p i f rien bent rien rien
 ;;
-  
 
+let sauvegarder p i f info fout =
+  let out = open_out fout in
+  let bent p t =
+    save_info (info p) out;
+    output_string out "\n";
+  in
+  faire p i f rien bent rien (fun p f -> close_out out)
--- roundabouts/modele/plateau.mli
+++ roundabouts/modele/plateau.mli
@@ -1,8 +1,34 @@
+open Objets
+
 type plateau;;
 (*un plateau de jeu contenant sections et distributions
 i.e. un automate*)
 
-val construire: Objets.section list -> Objets.distr list -> (int -> unit) list -> plateau;;
+type rond_point;;
+
+val rp_ent : rond_point -> section array;;
+
+val rp_sor : rond_point -> section array;;
+
+val rp_ron : rond_point -> section array;;
+
+val rp_dis : rond_point -> distr array;;
+(*acceder aux composantes de l'enregistrement*)
+  
+val creer_rond_point: int -> int -> int -> int -> int -> ( (voiture * int * section * section) option array -> section array -> section list array -> int -> unit)
+                      -> rond_point ;;
+(*creer_rond_point n l p vmi vme cmp cree un rond-point a n entrees/sorties
+de taille l, un anneau interne de taille n*p, des intersection de comportement
+de comportement cmp, avec une vitesse max interne(resp externe) de vmi (resp vma)*)
+
+val faire_itin: rond_point -> int -> int -> section list -> section list;;
+(*faire itin rp ne ns fin
+complete l'itineraire fin en lui ajoutant devant l'itineraire de l'entree ne a la sortie ns*)
+
+val pl_add_rp : plateau -> rond_point -> unit;;
+(*pl_add_rp pl rp ajoute le rond-point rp au plateau pl*)
+  
+val construire: section list -> distr list -> (int -> unit) list -> plateau;;
 (*construire s d sp construit le plateau p avec
 une liste de sections s
 une liste de distributions d
@@ -17,23 +43,23 @@ val iterer : plateau -> int -> unit;;
 val afficher : plateau -> unit;;
   (*affiche toutes les sections du plateau*)
 
-val imager : plateau -> (Objets.section*((int*int)*float*Graphics.color)) list -> unit;;
+val imager : plateau -> (section*((int*int)*float*Graphics.color)) list -> unit;;
    
-val spawn_car : int -> int -> int -> int -> Objets.section -> Objets.section list ->int -> unit
+val spawn_car : int -> int -> int -> int -> section -> section list ->int -> unit
 (*spawn_car per ph v pos sec itin t 
 fait apparaitre une voiture dans sec a la vitesse v position pos 
 d'itineraire itin si c'est le moment de faire apparaitre une voiture
 i.e. t == ph mod per*)
 
-val rnd_spawn_car : float -> int -> int -> Objets.section -> Objets.section list ->int -> unit;;
+val rnd_spawn_car : float -> int -> int -> section -> section list ->int -> unit;;
 (*rnd_spawn_car p v pos sec itin t version stochastique de spawn_car : fait apparaitre une voiture
 avec une probabilite de p *)
 
-val spawn_prog : (int -> float) -> int -> int -> Objets.section -> Objets.section list -> int -> unit ;;
+val spawn_prog : (int -> float) -> int -> int -> section -> section list -> int -> unit ;;
   (*spawn_prog f ... effectue un rnd_spawn_car avec une proba de (f t) 
 permet de faire une apparition croissante des éléments*)
 
-val faire : plateau -> int -> int -> (plateau -> unit) -> (plateau -> unit) -> (plateau -> unit) -> (plateau -> unit) -> unit;;
+val faire : plateau -> int -> int -> (plateau -> int -> unit) -> (plateau -> int -> unit) -> (plateau -> int -> unit) -> (plateau -> int -> unit) -> unit;;
 (*faire p i f dbt bent bsor fin : trivial par induction sur les termes du lambda calcul
 *)
 
@@ -45,9 +71,11 @@ val jouer : plateau -> int -> int -> unit;;
    en les affichant a chaque tour, l'horloge
 commencant a i inclus et finissant a f exclus*)
 
-val animer : plateau -> int -> int -> (Objets.section*((int*int)*float*Graphics.color)) list -> unit;;
+val animer : plateau -> int -> int -> (section*((int*int)*float*Graphics.color)) list -> unit;;
   (*animer p i f gr anime a l'aide du module Graphics en representant le plateau decrit par gr*)
 
-val modeliser : plateau -> int -> int -> (unit -> unit) -> unit;;
-  (*modeliser p i f info joue (f-i) iterations de l'automate, appliquant
-info a chaque tour*)
+val modeliser : plateau -> int -> int -> (plateau -> float list) -> unit;;
+  (*modeliser p i f info joue (f-i) iterations de l'automate, et affiche les informations collectees par info a chaque tour, espacees d'un retour a la ligne*)
+
+val sauvegarder : plateau -> int -> int -> (plateau -> float list) -> string -> unit;;
+  (*idem modeliser, mais au lieu de les afficher, les sauve dans le fichier donne en dernier argument*)
